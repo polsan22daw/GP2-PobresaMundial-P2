df087ba771e7de7188ca048ac5229eb5f81c68e8
polsan22daw <polsan22daw@proton.me>
2023-03-08 16:30:19 +0100

    about us

--- app/views/Home.js
+++ app/views/Home.js
@@ -6,11 +6,25 @@ const Home = () => {
         <View>
             <Text
                 style={{
-                fontSize: 30,
+                fontSize: 33,
                 textAlign: 'center',
                 marginTop: "20%"
                 }}
-                >Home Screen</Text>
+            >About Us</Text>
+            <Text
+                style={{
+                fontSize: 18,
+                textAlign: 'justify',
+                marginTop: "5%",
+                marginLeft: "10%",
+                marginRight: "10%"
+                }}
+                >
+                Els desenvolupadors d'aquest lloc web som Marc Pedemonte, Pol Sanz i Angel Torres. Som estudiants de DAW2, ens han 
+                demanat realitzar un projecte en comú utilitzant el framework d’Angular per desenvolupar. Aquest lloc web consisteix 
+                en mostrar gràficament dades de països de tot el món mitjançant taules amb dades o bé gràfics que permeten visualitzar 
+                millor les diferencies entre països.
+                </Text>
         </View>
     );
 };
