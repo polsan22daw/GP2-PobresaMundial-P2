653e936326bd4c2a99364ae6129da782a49cb2d4
polsan22daw <polsan22daw@proton.me>
2023-03-11 21:40:19 +0100

    instertar tabla pobresa sqllite

--- navigation.js
+++ navigation.js
@@ -1,20 +1,8 @@
 import React from "react";
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-// import { Icon } from 'react-native-elements'
-
 import Home from "./app/views/Home";
-// import Taulas from "./app/views/Taulas";
-import Grafics from "./app/views/Grafics";
-import Opcions from "./app/views/Opcions";
-
-
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Ionicons } from '@expo/vector-icons';
-import { Foundation } from '@expo/vector-icons';
-
-// import { createStackNavigator } from '@react-navigation/stack';
-// import Tabla1 from './app/views/taula1';
 import Tables from "./stackNavigation";
 
 const Tab = createBottomTabNavigator();
@@ -48,43 +36,10 @@ function MyTabs() {
                         ),
                     }}
                 />
-
-            <Tab.Screen
-                name="Grafics"
-                component={Grafics}
-                options={{
-                    tabBarLabel: 'Grafics',
-                    tabBarIcon: ({ color, size }) => (
-                        <Foundation name="graph-bar" size={24} color={color} />
-                    ),
-                }}
-            />
-            <Tab.Screen
-                name="Opcions"
-                component={Opcions}
-                options={{
-                    tabBarLabel: 'Opcions',
-                    tabBarIcon: ({ color, size }) => (
-                        <Ionicons name="options" size={24} color={color} />
-                    ),
-                }}
-            />
         </Tab.Navigator>
         
     );
 }
-
-// const Stack = createStackNavigator();
-
-// function Tables(){
-//     return (
-//                 <Stack.Navigator>
-//                     <Stack.Screen name="Taulas" component={Taulas} />
-//                     <Stack.Screen name="Taula1" component={Tabla1} />
-//                 </Stack.Navigator>
-//     );
-// }
-
 export default function Navigation() {
     return (
         <NavigationContainer>
--- stackNavigation.js
+++ stackNavigation.js
@@ -2,8 +2,8 @@ import React from "react";
 import { createStackNavigator } from '@react-navigation/stack';
 // import Tabla1 from './app/views/taula1';
 import Taulas from "./app/views/Taulas";
-import PovertyTable from "./app/views/taula1";
-import povertyData from "./assets/tabla1.json";
+import {TaulaPobresa} from "./app/views/taula1";
+// import povertyData from "./assets/tabla1.json";
 import EspervidaTable from "./app/views/taulaEspervida";
 import espervidaData from "./assets/espervida.json";
 import FertilitatTable from "./app/views/taulaFertilitat";
@@ -15,7 +15,7 @@ function Tables(){
     return (
                 <Stack.Navigator>
                     <Stack.Screen name=" " component={Taulas} options={{ headerShown: false }}/>
-                    <Stack.Screen name="Pobresa" component={() => <PovertyTable data={povertyData} />} />
+                    <Stack.Screen name="Pobresa" component={TaulaPobresa} />
                     <Stack.Screen name="Esperança de vida" component={() => <EspervidaTable data={espervidaData} />} />
                     <Stack.Screen name="Fertilitat" component={() => <FertilitatTable data={fertilitatData} />} />
                 </Stack.Navigator>
